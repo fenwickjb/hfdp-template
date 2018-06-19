6b604ae9d2d87cbf7fa2e8e5ee37ba78ae0a55f2
Jay Fenwick <fenwickjb@appstate.edu>
2018-06-18 22:23:17 -0400

    Initial library system

---
+++ src/Library.java
@@ -0,0 +1,66 @@
+
+import java.util.HashMap;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+
+public class Library {
+
+    private HashMap<String,Patron> patrons;
+
+    public Library() {
+	patrons = new HashMap<String,Patron>();
+    }
+
+    public void addPatron(String patronName) {
+	patrons.put(patronName, new Patron(patronName));
+    }
+
+    private boolean checkPatronAccount(Patron p) {
+	System.out.println("checking account...");
+	return true;
+    }
+
+    private String getSuggestion(String item) {
+	System.out.println("looking up suggestions...");
+	return "Suggested items...";
+    }
+
+    private void printReceipt(Patron p, String item, int days, String suggestion) {
+	SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");  
+	Date date = new Date();
+	String datetimeString = formatter.format(date);  	
+	
+	System.out.println("--------------------------------------");
+	System.out.println("--  L I B R A R Y     R E C E I P T --");
+	System.out.println();
+	System.out.println(" " + datetimeString);
+	System.out.println(" Patron: " + p.getName());
+	System.out.println(" Item: " + item);
+	Date due = new Date(date.getTime()+(days*24*60*60*1000));
+	formatter = new SimpleDateFormat("MM/dd/yyyy");  
+	datetimeString = formatter.format(due);  	
+	System.out.println(" Due: " + datetimeString);
+	System.out.println();
+	System.out.println(" Suggested title:");
+	System.out.println("   " + suggestion);
+	System.out.println("--------------------------------------");
+    }
+
+    private int getDuration(Patron p) {
+	return 14;
+    }
+
+    public void checkout(String item, String name) {
+	Patron p = patrons.get(name);
+
+	boolean okayToCheckout = checkPatronAccount(p);
+
+	if (okayToCheckout) {
+	    int days = getDuration(p);
+	    p.checkout(item);
+	    String s = getSuggestion(item);
+	    printReceipt(p, item, days, s);
+	}
+    }
+
+}
